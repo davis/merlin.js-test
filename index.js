26f78ecd93c2deea656c4419e5b83d9845568aa2
Davis Kim <davis@blackbird.am>
2016-02-10 15:00:09 -0800

    update app to do a search

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var Blackbird = require('merlin.js');
 
 var msg = 'Running merlin.js version ' + Blackbird.VERSION;
+
+var engine = Blackbird.engine({
+  protocol: 'http',
+  cluster: 'search-demo.bbird.io',
+  company: 'blackbird',
+  environment: 'prod',
+  instance: 'blackbird'
+});
+
+engine.search().then(function (res) {console.log(res.body)});
+
 document.write(msg);
